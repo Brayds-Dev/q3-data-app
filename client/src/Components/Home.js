f13fcc5cca069d5c138d165394a606a282b1cd9c
Dwain Aiolupotea <d.aiolupo@gmail.com>
2022-09-12 16:17:52 +1200

    add delete operations

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,37 +1,52 @@
-import React, {useState, useEffect} from 'react'
-import Axios from 'axios';
-import './Home.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import "./Home.css";
 
 function Home() {
-    
-    const [articleList, setArticleList] = useState([]);
+  const [articleList, setArticleList] = useState([]);
 
-    const [textSearch, setTextSearch] = useState('');
-  
-    console.log(textSearch);
+  const [textSearch, setTextSearch] = useState("");
 
-    // Using Axios send an http request to one of our custom routes created in the server index.js file
-    useEffect(()=> {
-        Axios.get("http://localhost:3001/read").then((response)=> {
-          setArticleList(response.data);
-        });
-      }, []);
+  console.log(textSearch);
+
+  // Using Axios send an http request to one of our custom routes created in the server index.js file
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      setArticleList(response.data);
+    });
+  }, []);
+
+  const deleteArticle = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`);
+    window.location.reload();
+  };
 
   return (
     <div>
-        <h1>Article List</h1>
-        <input type="search" name='searchTerm' placeholder='title keyword....' onChange={(event)=>{setTextSearch(event.target.value)}} />
-        
-        {articleList.filter(({name})=>name.toLowerCase().includes(textSearch.toLowerCase())).map((value, key) => {
-            return (
-                
-                <div key={key}>
-                    <h3>{value.name}</h3>
-                </div>
-            )
+      <h1>Article List</h1>
+      <input
+        type="search"
+        name="searchTerm"
+        placeholder="title keyword...."
+        onChange={(event) => {
+          setTextSearch(event.target.value);
+        }}
+      />
+
+      {articleList
+        .filter(({ name }) =>
+          name.toLowerCase().includes(textSearch.toLowerCase())
+        )
+        .map((value, key) => {
+          return (
+            <div key={key}>
+              <h3>{value.name}</h3>
+              <button onClick={() => deleteArticle(value._id)}>Delete</button>
+            </div>
+          );
         })}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- server/index.js
+++ server/index.js
@@ -1,100 +1,105 @@
-
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const ArticleModel = require('./models/Article.js');
+const ArticleModel = require("./models/Article.js");
 
-mongoose.connect("mongodb+srv://bdaw211:ZmAKUg7kKKEVltlC@teamproject.o3vp87l.mongodb.net/schooldb?retryWrites=true&w=majority", {
+mongoose.connect(
+  "mongodb+srv://bdaw211:ZmAKUg7kKKEVltlC@teamproject.o3vp87l.mongodb.net/schooldb?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-});
+  }
+);
 
 // API to retrieve all data from the database
-app.get('/read', async (req, res) => {
-    ArticleModel.find({}, (error, result) => {
-        if (error){
-            res.send(error);
-        }
-        res.send(result);
-    });
+app.get("/read", async (req, res) => {
+  ArticleModel.find({}, (error, result) => {
+    if (error) {
+      res.send(error);
+    }
+    res.send(result);
+  });
 });
 
 // API to retrieve all documents with the category type 'ARTS'
-app.get('/read/arts', async (req, res) => {
-    ArticleModel.find({category: 'Arts'}, (error, result) => {
-        if (error){
-            res.send(error);
-        }
-        res.send(result);
-    });
+app.get("/read/arts", async (req, res) => {
+  ArticleModel.find({ category: "Arts" }, (error, result) => {
+    if (error) {
+      res.send(error);
+    }
+    res.send(result);
+  });
 });
 
 // API to retrieve all documents with the category type 'Methematics'
-app.get('/read/mathematics', async (req, res) => {
-    ArticleModel.find({category: 'Mathematics'}, (error, result) => {
-        if (error){
-            res.send(error);
-        }
-        res.send(result);
-    });
+app.get("/read/mathematics", async (req, res) => {
+  ArticleModel.find({ category: "Mathematics" }, (error, result) => {
+    if (error) {
+      res.send(error);
+    }
+    res.send(result);
+  });
 });
 
 // API to retrieve all documents with the category type 'Technology'
-app.get('/read/technology', async (req, res) => {
-    ArticleModel.find({category: 'Technology'}, (error, result) => {
-        if (error){
-            res.send(error);
-        }
-        res.send(result);
-    });
+app.get("/read/technology", async (req, res) => {
+  ArticleModel.find({ category: "Technology" }, (error, result) => {
+    if (error) {
+      res.send(error);
+    }
+    res.send(result);
+  });
 });
 
 // API to create a new db entry or POST
-app.post('/create', async (req, res) => {
+app.post("/create", async (req, res) => {
+  //gets the data from the front end form
+  const category = req.body.category;
+  const type = req.body.type;
+  const name = req.body.name;
+  const born = req.body.born;
+  const died = req.body.died;
+  const nationality = req.body.nationality;
+  const knownFor = req.body.knownFor;
+  const notableWork = req.body.notableWork;
+  const about = req.body.about;
 
-    //gets the data from the front end form
-    const category = req.body.category;
-    const type = req.body.type;
-    const name = req.body.name;
-    const born = req.body.born;
-    const died = req.body.died;
-    const nationality = req.body.nationality;
-    const knownFor = req.body.knownFor;
-    const notableWork = req.body.notableWork;
-    const about = req.body.about;
+  //creates a new article with the above data
+  const article = new ArticleModel({
+    category: category,
+    type: type,
+    name: name,
+    born: born,
+    died: died,
+    nationality: nationality,
+    knownFor: knownFor,
+    notableWork: notableWork,
+    about: about,
+  });
 
-    //creates a new article with the above data
-    const article = new ArticleModel(
-        {
-            category: category, 
-            type: type,
-            name: name,
-            born: born,
-            died: died,
-            nationality: nationality,
-            knownFor: knownFor,
-            notableWork: notableWork,
-            about: about,
-        })
-    
-    //uses try-catch to attempt to save the data to the db, logging any errors.
-    try {
-        await article.save();
-        res.send("inserted data");
-    }
-    catch(err){
-        console.log(err);
-        return(err);
-    }
+  //uses try-catch to attempt to save the data to the db, logging any errors.
+  try {
+    await article.save();
+    res.send("inserted data");
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 });
 
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await ArticleModel.findByIdAndRemove(id).exec();
+  res.send("deleted");
+});
 
 // Set port from local host to run backend server on
 app.listen(3001, () => {
-    console.log("Server running on port 3001....")
-    console.log("ctrl 'C' to stop the server")
+  console.log("Server running on port 3001....");
+  console.log("ctrl 'C' to stop the server");
 });
