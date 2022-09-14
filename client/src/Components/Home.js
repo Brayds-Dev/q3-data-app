ec01747f42a906db9edf73c0034e090118cca187
John Thompson Wright <jwri211@mywhitecliffe.com>
2022-09-14 15:04:06 +1200

    make article detail view work a bit

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,16 +1,16 @@
 import React, {useState, useEffect} from 'react'
 import Axios from 'axios';
 import './Home.css';
+import {Link,} from 'react-router-dom';
 
 function Home() {
     
     const [articleList, setArticleList] = useState([]);
-
     const [textSearch, setTextSearch] = useState('');
-  
-    console.log(textSearch);
 
-    // Using Axios send an http request to one of our custom routes created in the server index.js file
+    //console.log(textSearch);
+
+    //Using Axios send an http request to one of our custom routes created in the server index.js file
     useEffect(()=> {
         Axios.get("http://localhost:3001/read").then((response)=> {
           setArticleList(response.data);
@@ -20,13 +20,16 @@ function Home() {
   return (
     <div>
         <h1>Article List</h1>
-        <input type="search" name='searchTerm' placeholder='title keyword....' onChange={(event)=>{setTextSearch(event.target.value)}} />
+        <input type="search" 
+               name='searchTerm' 
+               placeholder='title keyword....' 
+               onChange={(event)=>{
+                setTextSearch(event.target.value)}} />
         
         {articleList.filter(({name})=>name.toLowerCase().includes(textSearch.toLowerCase())).map((value, key) => {
             return (
-                
                 <div key={key}>
-                    <h3>{value.name}</h3>
+                    <h3><Link to={{pathname: `${value._id}`}}>{value.name}</Link></h3>
                 </div>
             )
         })}
