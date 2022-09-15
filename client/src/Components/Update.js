a7bb6d50f19df9910e1d7c1d225d14f8a1845801
John Thompson Wright <jwri211@mywhitecliffe.com>
2022-09-15 16:45:33 +1200

    individual fields update independantly

--- client/src/Components/Update.js
+++ client/src/Components/Update.js
@@ -6,91 +6,74 @@ import {useParams,} from 'react-router-dom'
 //We want to be able to add missing fields as well as update current ones.
 function Update() {
 
-    //Constants for the specific article
-    const {articleID} = useParams();
-    const [article, setArticle] = useState([]);
+  const {articleID} = useParams(); //get the article id from the url
+  const [article, setArticle] = useState([]); //set the article object, based on id above.
 
-    //constants for possible update values.
-    const [category, setCategory] = useState();
-    const [type, setType] = useState();
-    //const [name, setName] = useState();
+  //Axios call when component first loads to get the article.
+  useEffect(()=> {
+      //Ask Axios politely to get just the article with this ID number.
+      Axios.get(`http://localhost:3001/read/${articleID}`).then((response)=> {
+        //assign the result to the the article variable
+        setArticle(response.data);
+      });
+    }, []);
 
-    //Axios call when component first loads to get the article.
-    useEffect(()=> {
-        //Ask Axios politely to get just the article with this ID number.
-        Axios.get(`http://localhost:3001/read/${articleID}`).then((response)=> {
-          //assign the result to the the article variable
-          setArticle(response.data);
-        });
-      }, []);
-
-    //assigns all the fields to article but - 
-    //???how to only update the fields that have changed if not all change???
-    const assignNewArticleValues = () =>{
-      article.category = category;
-      article.type = type;
-      //testing values get passed in properly THIS WORKS
-      console.log("New category should be: " + article.category)
-      console.log("New category should be: " + article.type)
+  
+  //Handle form submission
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      
+      //call axios, pass in all the fields of the article object (except id and vers.)
+      Axios.put(`http://localhost:3001/update/${articleID}`, 
+      {
+        //send all article object fields.
+        category : article.category,
+        type : article.type
+      })
+      .then(function (response) 
+      {
+        console.log(response);
+        alert('Article created'); //pop up
+        document.location.href="/"; //return home
+      })
     }
-    //we may not need to assign the values to the article object because we already have the id
-    //so we just pass in the changed fields to the axios call.
 
-    //Handle form submission
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //set the new values to article (???)
-        //assignNewArticleValues();
-        
-        //call axios, pass in all the fields of the article object (except id and vers.)
-        Axios.put(`http://localhost:3001/update/${articleID}`, 
-        {
-          //just give category and type now.
-          category : category,
-          type : type
-        })
-        .then(function (response) 
-        {
-          console.log(response);
-          alert('Article created'); //pop up
-          document.location.href="/"; //return to home page
-        })
-      }
-
-    return(
+  return(
+    <div>
+        {/**Title */}
         <div>
-            {/**Title */}
-            <div>
-                <h1>Update existing article</h1>
-            </div>
-            
-            {/**Form for updating each component. */}
-            <form onSubmit={e => { handleSubmit(e) }}> 
-                {/**Each input is a different possible field for the article. */}
-                {/**Currently testing updates on only two fields. */}
-                <label>
-                    {"New category: "} 
-                <input type="text" 
-                  placeholder={article.category}
-                  //onchange set the article.category not this function const category.
-                  onChange={e => setCategory(e.target.value)}
-                  name="category" />
-                </label>
-                <br></br>
-
-                 <label>
-                    {"New Type: "} 
-                <input type="text" 
-                  placeholder={article.type}
-                  onChange={e => setType(e.target.value)}
-                  name="type" />
-                </label>
-                <br></br> 
-
-            <input type="submit" value="Submit" />
-          </form>
+            <h1>Update existing article: </h1>
         </div>
         
-    )
+        {/**Form for updating each component. */}
+        <form onSubmit={e => { handleSubmit(e) }}> 
+            {/**Each input is a different possible field for the article. */}
+            {/**Currently testing updates on only two fields. */}
+            {/**Each change directly modifies the fields of the article object.*/}
+            <label>
+              {"New category: "} 
+              <input type="text" 
+              placeholder={article.category}
+              //onchange updates the article category
+              onChange={e => {article.category = e.target.value;}}
+              />
+            </label>
+            <br></br>
+
+              <label>
+              {"New Type: "} 
+              <input type="text" 
+              placeholder={article.type}
+              //onchange updates the article type
+              onChange={e => {article.type = e.target.value;}}
+              />
+            </label>
+            <br></br> 
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+      
+  )
 }
 export default Update
