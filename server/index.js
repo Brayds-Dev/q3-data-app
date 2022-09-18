85012ae2986cad9cf1349e69c6c0f1521e40268b
Brayds_Dev <braydendawson91@gmail.com>
2022-09-19 11:04:24 +1200

    Add .env to folder

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config({path: "./environment/.env"});
 const app = express();
 
 app.use(express.json());
